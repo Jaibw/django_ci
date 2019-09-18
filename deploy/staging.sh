52869c74e555feb1fa03c3a8844eeaa9f9359cda
Jai <jai.unix@gmail.com>
2019-09-18 15:00:03 +0530

    update

--- deploy/staging.sh
+++ deploy/staging.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# ssh -i ~/auth/key.pem ubuntu@13.233.117.16 <<EOF
-#   cd django_ci
-#   git pull
-#   source ../env/bin/activate
-#   pip install -r requirements.txt
-#   ./manage.py migrate
-#   sudo supervisorctl restart djangoci
-#   exit
-# EOF
+ssh -i ~/auth/key.pem ubuntu@13.233.194.158 <<EOF
+  cd django_ci
+  git pull
+  source ../env/bin/activate
+  pip install -r requirements.txt
+  ./manage.py migrate
+  sudo supervisorctl restart djangoci
+  exit
+EOF
