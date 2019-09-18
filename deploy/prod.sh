1284d1695f18dbe1aacb165cdda89078065f8afc
Jai <jai.unix@gmail.com>
2019-09-18 14:46:50 +0530

    update

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ssh -i ~/auth/key.pem ubuntu@13.233.117.16 <<EOF
-  cd django_ci/
+  cd django_ci
   git pull
   source ../env/bin/activate
   pip install -r requirements.txt
--- deploy/staging.sh
+++ deploy/staging.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ssh -i ~/auth/key.pem ubuntu@13.233.117.16 <<EOF
-  cd devops_django
+  cd django_ci
   git pull
   source ../env/bin/activate
   pip install -r requirements.txt
   ./manage.py migrate
-  sudo supervisorctl restart devopsdjango
+  sudo supervisorctl restart djangoci
   exit
 EOF
