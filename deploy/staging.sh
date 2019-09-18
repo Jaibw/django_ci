59ccb36f82facf0c332413d93de45ed2cb2e839f
Jai <jai.unix@gmail.com>
2019-09-18 14:36:58 +0530

    update

---
+++ deploy/staging.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ssh -i auth/key.pem ubuntu@13.233.117.16 <<EOF
+  cd devops_django
+  git pull
+  source ../env/bin/activate
+  pip install -r requirements.txt
+  ./manage.py migrate
+  sudo supervisorctl restart devopsdjango
+  exit
+EOF
